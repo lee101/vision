146274065c6e36823959a7129926ec22bf8f8976
Lee Penkman <leepenkman@gmail.com>
2016-02-20 15:07:26 +1300

    add url handler

--- main.py
+++ main.py
@@ -1,8 +1,48 @@
 # !/usr/bin/env python
+import logging
 import os
+from google.appengine.api import urlfetch
 
 import webapp2
 import jinja2
+
+import lib.cloudstorage as gcs
+
+
+# Retry can help overcome transient urlfetch or GCS issues, such as timeouts.
+my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
+                                          max_delay=5.0,
+                                          backoff_factor=2,
+                                          max_retry_period=15)
+# All requests to GCS using the GCS client within current GAE request and
+# current thread will use this retry params as default. If a default is not
+# set via this mechanism, the library's built-in default will be used.
+# Any GCS client function can also be given a more specific retry params
+# that overrides the default.
+# Note: the built-in default is good enough for most cases. We override
+# retry_params here only for demo purposes.
+gcs.set_default_retry_params(my_default_retry_params)
+
+IMG_BUCKET = '/visiontestimages/'
+
+
+def saveUrl(url, title):
+    '''
+    saves object at url in cloud storage
+    '''
+    response = urlfetch.fetch(url)
+    if response.status_code == 200:
+        write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+        if not response.headers['content-type']:
+            logging.error('no content-type header returned')
+            logging.error(response.headers)
+        gcs_file = gcs.open(title,
+                            'w',
+                            content_type=response.headers['content-type'],
+                            options={'x-goog-acl': 'public-read'},
+                            retry_params=write_retry_params)
+        gcs_file.write(response.content)
+        gcs_file.close()
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -20,6 +60,16 @@ class BaseHandler(webapp2.RequestHandler):
 
         template = JINJA_ENVIRONMENT.get_template(view_name)
         self.response.write(template.render(template_values))
+
+
+class GetFromUrlHandler(webapp2.RequestHandler):
+    def get(self, url):
+        saveUrl(url, url.hash())
+
+
+        self.response.headers['Content-Type'] = 'text/xml'
+        template = JINJA_ENVIRONMENT.get_template("/templates/sitemap.xml")
+        self.response.write()
 
 
 class MainHandler(BaseHandler):
